491521fbf68a658316403ed4886b92285bf189bb
zhangyi02 <zhangyi02@daojia.com>
2018-08-09 00:50:13 +0800

    1

---
+++ frame/src/main/java/com/easyzhang/frame/init/DispatcherServlet.java
@@ -0,0 +1,11 @@
+package com.easyzhang.frame.init;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
---
+++ frame/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.easyzhang.frame {
+   requires servlet.api;
+   exports com.easyzhang.frame.init;
+}
