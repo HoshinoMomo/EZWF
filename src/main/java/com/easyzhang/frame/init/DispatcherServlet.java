84779482c8030a4f97efe8e3086807c67205f139
zhangyi02 <zhangyi02@daojia-inc.com>
2018-11-07 10:57:35 +0800

    什么是ioc

--- src/main/java/com/easyzhang/frame/init/DispatcherServlet.java
+++ src/main/java/com/easyzhang/frame/init/DispatcherServlet.java
@@ -32,19 +32,19 @@ public class DispatcherServlet extends HttpServlet {
     /**
      * 这里写一个list集合，用于存放扫描出来所有的文件的类名
      */
-    public static List<String> classNames = new ArrayList<>();
+    private static List<String> classNames = new ArrayList<>();
 
     /**
      * 这里定义一个IOC的Map容器，用于存放实体类对象
      */
-    public static Map<String,Object> iocmap = new HashMap<>();
+    private static Map<String,Object> iocmap = new HashMap<>();
 
     /**
      * 这里再定义一个Map容器，用来记录，
      * 使用的是ConcurrentHashMap是线程安全的,
      * 这里定义的这个Map是用来记录  HandlerMapping，处理器映射器的
      */
-    ConcurrentHashMap<String,Method> hm = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String,Method> hm = new ConcurrentHashMap<>();
 
     @Override
     public void init() throws ServletException {
